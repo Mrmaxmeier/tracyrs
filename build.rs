8369af17e5e2332a068a22a794cec3d7da5b4af1
Mrmaxmeier <Mrmaxmeier@gmail.com>
2020-01-09 13:23:47 +0100

    tracy feature flags as #[cfg()]

--- build.rs
+++ build.rs
@@ -3,11 +3,25 @@ use cc::Build;
 fn main() {
     println!("cargo:rustc-link-lib=dl");
     println!("cargo:rustc-link-lib=pthread");
-    Build::new()
-        .cpp(true)
-        .shared_flag(true)
-        .pic(true)
-        .define("TRACY_ENABLE", "1")
+
+    let mut build = Build::new();
+    build.cpp(true);
+    build.shared_flag(true);
+    build.pic(true);
+
+    #[cfg(tracy_enable)]
+    build.define("TRACY_ENABLE", "1");
+
+    #[cfg(tracy_on_demand)]
+    build.define("TRACY_ON_DEMAND", "1");
+
+    #[cfg(tracy_no_exit)]
+    build.define("TRACY_NO_EXIT", "1");
+
+    #[cfg(tracy_no_broadcast)]
+    build.define("TRACY_NO_BROADCAST", "1");
+
+    build
         .flag("-Wno-implicit-fallthrough")
         .flag("-Wno-unused-parameter")
         .flag("-Wno-unused-function")
